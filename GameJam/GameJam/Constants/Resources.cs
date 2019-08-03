001683952b544f02928cc79bd24c944fc5fe23a2
Philip Rader <pjrader1@yahoo.com>
2019-08-03 15:53:34 -0700

    Change ship texture

--- GameJam/GameJam/Constants/Resources.cs
+++ GameJam/GameJam/Constants/Resources.cs
@@ -2,7 +2,7 @@
 {
     public class Resources
     {
-        public static readonly string TEXTURE_PLAYER_SHIP = "textures/ship";
+        public static readonly string TEXTURE_PLAYER_SHIP = "textures/PlayerShip";
         public static readonly string TEXTURE_PLAYER_SHIELD = "textures/shield";
     }
 }
