678c3ada6d6bf2af0a7ee2b44c9b48200b0ca536
Keith Petitt <keaver021@gmail.com>
2019-08-04 08:03:42 -0700

    Added wall collision for PlayerShip

---
+++ GameJam/GameJam/Components/EdgeComponent.cs
@@ -0,0 +1,17 @@
+﻿using Audrey;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameJam.Components
+{
+    public class EdgeComponent : IComponent
+    {
+        public Vector2 Normal;
+        public EdgeComponent(Vector2 norm)
+        {
+            Normal = norm;
+        }
+    }
+}
