f797b1e186398bb9e60f8b4b27b790669e6e92ef
Unknown <pjrader1@yahoo.com>
2019-11-25 14:22:32 -0800

    Add MonoGame UWP project

---
+++ GameJam/GameJam.UWP/GamePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MonoGame.Framework;
+using System;
+using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace GameJam.UWP
+{
+    public sealed partial class GamePage : Page
+    {
+        readonly GameManager GameManager;
+
+        public GamePage()
+        {
+            try
+            {
+                InitializeComponent();
+                GameManager = XamlGame<GameManager>.Create(string.Empty,
+                    Window.Current.CoreWindow, swapChainPanel);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Debug.WriteLine(ex.InnerException);
+            }
+        }
+    }
+}
