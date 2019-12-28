f3167953b6fba6ca5ead7116e41b41d6c038d74a
Philip Rader <pjrader1@yahoo.com>
2019-12-28 15:30:11 -0800

    Add basic animations for ships spawning

--- GameJam/GameJam/Entities/LaserEnemyEntity.cs
+++ GameJam/GameJam/Entities/LaserEnemyEntity.cs
@@ -1,6 +1,7 @@
 ﻿using Audrey;
 using GameJam.Components;
 using GameJam.Processes.Enemies;
+using GameJam.Processes.Entities;
 using Microsoft.Xna.Framework;
 using System;
 
@@ -8,26 +9,36 @@ namespace GameJam.Entities
 {
     public static class LaserEnemyEntity
     {
+        private static Vector2[] GetOuterPoints()
+        {
+            return new Vector2[]{ new Vector2(4, 0),
+                new Vector2(3, 1),
+                new Vector2(-3, 3),
+                new Vector2(-5, 1),
+                new Vector2(-5, -1),
+                new Vector2(-3, -3),
+                new Vector2(3, -1)
+            };
+        } 
+
         public static Entity CreateSpriteOnly(Engine engine)
+        {
+            return CreateSpriteOnly(engine, Vector2.Zero, 0);
+        }
+        public static Entity CreateSpriteOnly(Engine engine, Vector2 position, float angle)
         {
             Entity entity = engine.CreateEntity();
 
             entity.AddComponent(new TransformComponent());
             entity.AddComponent(new VectorSpriteComponent(new RenderShape[] {
-                new PolyRenderShape(new Vector2[]{ new Vector2(4, 0),
-                    new Vector2(3, 1),
-                    new Vector2(-3, 3),
-                    new Vector2(-5, 1),
-                    new Vector2(-5, -1),
-                    new Vector2(-3, -3),
-                    new Vector2(3, -1)
-                    }, 0.35f, CVars.Get<Color>("color_laser_enemy"), PolyRenderShape.PolyCapStyle.Filled, true),
+                new PolyRenderShape(GetOuterPoints(), 0.35f, CVars.Get<Color>("color_laser_enemy"), PolyRenderShape.PolyCapStyle.Filled, true),
                 new PolyRenderShape(new Vector2[]{ new Vector2(-3, 3),
                     new Vector2(2, 0),
                     new Vector2(-3, -3)
                     }, 0.2f, CVars.Get<Color>("color_laser_enemy"), PolyRenderShape.PolyCapStyle.Filled)
             }));
-            entity.GetComponent<VectorSpriteComponent>().RenderGroup = Constants.Render.RENDER_GROUP_GAME_ENTITIES;
+            entity.GetComponent<VectorSpriteComponent>().RenderGroup = Constants.Render.RENDER_GROUP_GAME_ENTITIES; entity.GetComponent<TransformComponent>().SetPosition(position);
+            entity.GetComponent<TransformComponent>().SetRotation(angle);
             entity.GetComponent<TransformComponent>().ChangeScale(CVars.Get<float>("laser_enemy_size"), true);
             entity.AddComponent(new ColoredExplosionComponent(CVars.Get<Color>("color_laser_enemy")));
 
@@ -36,11 +47,14 @@ namespace GameJam.Entities
 
         public static Entity Create(Engine engine, ProcessManager processManager, Vector2 position, float angle)
         {
-            Entity entity = CreateSpriteOnly(engine);
+            Entity entity = CreateSpriteOnly(engine, position, angle);
+            return AddBehavior(engine, entity, processManager);
+        }
 
-            entity.GetComponent<TransformComponent>().SetPosition(position);
-            entity.GetComponent<TransformComponent>().SetRotation(angle);
+        public static Entity AddBehavior(Engine engine, Entity entity, ProcessManager processManager)
+        {
             entity.AddComponent(new RotationComponent(CVars.Get<float>("laser_enemy_rotational_speed")));
+            float angle = entity.GetComponent<TransformComponent>().Rotation;
             entity.AddComponent(new MovementComponent(new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle)), 0));
             entity.AddComponent(new LaserEnemyComponent());
             entity.AddComponent(new EnemyComponent());
@@ -81,7 +95,16 @@ namespace GameJam.Entities
 
         public static void Spawn(Engine engine, ProcessManager processManager, Vector2 position, float angle)
         {
-            Create(engine, processManager, position, angle);
+            Entity entity = CreateSpriteOnly(engine, position, angle);
+            processManager.Attach(new EntityScaleProcess(engine,
+                entity,
+                1,
+                0,
+                CVars.Get<float>("laser_enemy_size"),
+                Easings.Functions.SineEaseOut)).SetNext(new DelegateProcess(() =>
+                {
+                    AddBehavior(engine, entity, processManager);
+                }));
         }
     }
 }
--- GameJam/GameJam/Entities/ShootingEnemyEntity.cs
+++ GameJam/GameJam/Entities/ShootingEnemyEntity.cs
@@ -1,6 +1,7 @@
 ﻿using Audrey;
 using GameJam.Components;
 using GameJam.Processes.Enemies;
+using GameJam.Processes.Entities;
 using Microsoft.Xna.Framework;
 using System;
 
@@ -8,23 +9,35 @@ namespace GameJam.Entities
 {
     public static class ShootingEnemyEntity
     {
+        private static Vector2[] GetPoints()
+        {
+            return new Vector2[]{
+                new Vector2(-2, -5),
+                new Vector2(-2, 5),
+                new Vector2(2, 4),
+                new Vector2(1, 1),
+                new Vector2(4, 0),
+                new Vector2(1, -1),
+                new Vector2(2, -4)
+            };
+        }
+
         public static Entity CreateSpriteOnly(Engine engine)
+        {
+            return CreateSpriteOnly(engine, Vector2.Zero, 0);
+        }
+        public static Entity CreateSpriteOnly(Engine engine, Vector2 position, float angle)
         {
             Entity entity = engine.CreateEntity();
 
             entity.AddComponent(new TransformComponent());
             entity.AddComponent(new VectorSpriteComponent(new RenderShape[] {
-                new PolyRenderShape(new Vector2[]{ new Vector2(-2, -5),
-                    new Vector2(-2, 5),
-                    new Vector2(2, 4),
-                    new Vector2(1, 1),
-                    new Vector2(4, 0),
-                    new Vector2(1, -1),
-                    new Vector2(2, -4)
-                    }, 0.3f, CVars.Get<Color>("color_shooting_enemy"), PolyRenderShape.PolyCapStyle.Filled, true)
+                new PolyRenderShape(GetPoints(), 0.3f, CVars.Get<Color>("color_shooting_enemy"), PolyRenderShape.PolyCapStyle.Filled, true)
             }));
             entity.GetComponent<VectorSpriteComponent>().RenderGroup = Constants.Render.RENDER_GROUP_GAME_ENTITIES;
             entity.GetComponent<TransformComponent>().ChangeScale(CVars.Get<float>("shooting_enemy_size"), true);
+            entity.GetComponent<TransformComponent>().SetPosition(position);
+            entity.GetComponent<TransformComponent>().SetRotation(angle);
             entity.AddComponent(new ColoredExplosionComponent(CVars.Get<Color>("color_shooting_enemy")));
 
             return entity;
@@ -32,12 +45,16 @@ namespace GameJam.Entities
 
         public static Entity Create(Engine engine, Vector2 position, ProcessManager processManager, float angle)
         {
-            Entity entity = CreateSpriteOnly(engine);
+            Entity entity = CreateSpriteOnly(engine, position, angle);
+            AddBehavior(engine, entity, processManager);
+            return entity;
+        }
 
-            entity.GetComponent<TransformComponent>().SetPosition(position);
-            entity.GetComponent<TransformComponent>().SetRotation(angle);
+        public static Entity AddBehavior(Engine engine, Entity entity, ProcessManager processManager)
+        {
             entity.AddComponent(new ShootingEnemyComponent(CVars.Get<int>("shooting_enemy_projectile_ammo")));
             entity.AddComponent(new RotationComponent(CVars.Get<float>("shooting_enemy_rotational_speed")));
+            float angle = entity.GetComponent<TransformComponent>().Rotation;
             entity.AddComponent(new MovementComponent(new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle)), CVars.Get<float>("shooting_enemy_speed")));
             entity.AddComponent(new EnemyComponent());
 
@@ -59,7 +76,16 @@ namespace GameJam.Entities
 
         public static void Spawn(Engine engine, ProcessManager processManager, Vector2 position, float angle)
         {
-            Create(engine, position, processManager, angle);
+            Entity entity = CreateSpriteOnly(engine, position, angle);
+            processManager.Attach(new EntityScaleProcess(engine,
+                entity,
+                1,
+                0,
+                CVars.Get<float>("chasing_enemy_size"),
+                Easings.Functions.SineEaseOut)).SetNext(new DelegateProcess(() =>
+                {
+                    AddBehavior(engine, entity, processManager);
+                }));
         }
     }
 }
