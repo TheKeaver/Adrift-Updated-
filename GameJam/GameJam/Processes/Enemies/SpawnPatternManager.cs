208a2c93035b9524cd22d55cef8e2abf7d418473
Keith Petitt <keaver021@gmail.com>
2020-01-04 16:28:26 -0800

    Spawn Patterns Start

---
+++ GameJam/GameJam/Processes/Enemies/SpawnPatternManager.cs
@@ -0,0 +1,26 @@
+﻿using Audrey;
+using GameJam.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameJam.Processes.Enemies
+{
+    public class SpawnPatternManager : IntervalProcess
+    {
+        readonly Engine Engine;
+        readonly MTRandom random = new MTRandom();
+        readonly ProcessManager ProcessManager;
+
+        public SpawnPatternManager(Engine engine, ProcessManager processManager) : base(2.0f)
+        {
+            Engine = engine;
+            ProcessManager = processManager;
+        }
+
+        protected override void OnTick(float interval)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
