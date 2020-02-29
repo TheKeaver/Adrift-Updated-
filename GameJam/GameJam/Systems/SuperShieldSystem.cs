fda9d6e4382e0cd1991b933968722f062953e5c0
Keith Petitt <keaver021@gmail.com>
2020-02-29 10:01:10 -0800

    Small changes

--- GameJam/GameJam/Systems/SuperShieldSystem.cs
+++ GameJam/GameJam/Systems/SuperShieldSystem.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Audrey;
+using GameJam.Components;
+using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +9,36 @@ namespace GameJam.Systems
 {
     public class SuperShieldSystem : BaseSystem
     {
+        readonly Family playerFamily = Family.All(typeof(PlayerComponent), typeof(PlayerShipComponent)).Get();
+        readonly ImmutableList<Entity> playerEntities;
+
+        public SuperShieldSystem(Engine engine) : base(engine)
+        {
+            playerEntities = engine.GetEntitiesFor(playerFamily);
+        }
+
         public override void Update(float dt)
         {
-            throw new NotImplementedException();
+            foreach (Entity player in playerEntities)
+            {
+                UpdateSuperShieldFromInput(player, dt);
+            }
+        }
+
+        private void UpdateSuperShieldFromInput(Entity player, float dt)
+        {
+            PlayerComponent play = player.GetComponent<PlayerComponent>();
+            PlayerShipComponent ship = player.GetComponent<PlayerShipComponent>();
+
+            if (play.Player.InputMethod.GetSnapshot().SuperShield == true)
+            {
+                if (ship.SuperShieldMeter > 0)
+                {
+                    ship.SuperShieldMeter -= CVars.Get<float>("player_super_shield") * dt;
+                }
+            }
+            else
+                ship.SuperShieldMeter = MathHelper.Min(ship.SuperShieldMeter + CVars.Get<float>("player_super_shield_regen_rate") * dt, CVars.Get<float>("player_super_shield_min"));
         }
     }
 }
